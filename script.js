6d2fe32eb2dffb447f9b5f15b5c842d060de98e3
Clement <perso@clementhurel.fr>
2022-03-12 10:34:14 +0900

    RPS

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const container = document.getElementById("container");
 let rows = document.getElementsByClassName("gridRow");
 let cells = document.getElementsByClassName("cell");
 
-
+defaultGrid();
 
 // Creates a default grid sized 16x16
 function defaultGrid() {
@@ -13,6 +13,7 @@ function defaultGrid() {
 // Takes (rows, columns) input and makes a grid
     // Creates rows
 function makeRows(rowNum) {
+    console.log('bob')
     for (r = 0; r < rowNum; r++) {
         let row = document.createElement("div");
         container.appendChild(row).className = "gridRow";
@@ -21,6 +22,7 @@ function makeRows(rowNum) {
 
 // Creates columns
 function makeColumns(cellNum) {
+    console.log('bob2')
     for (i = 0; i < rows.length; i++) {
         for (j = 0; j < cellNum; j++) {
             let newCell = document.createElement("div");
