da960621886183c45a847c7bc100aacf036c2889
Clement <perso@clementhurel.fr>
2022-03-12 10:39:41 +0900

    RPS

--- script.js
+++ script.js
@@ -1,33 +1,13 @@
 const container = document.getElementById("container");
-let rows = document.getElementsByClassName("gridRow");
-let cells = document.getElementsByClassName("cell");
 
-defaultGrid();
-
-// Creates a default grid sized 16x16
-function defaultGrid() {
-    makeRows(16);
-    makeColumns(16);
-}
-
-// Takes (rows, columns) input and makes a grid
-    // Creates rows
-function makeRows(rowNum) {
-    console.log('bob')
-    for (r = 0; r < rowNum; r++) {
-        let row = document.createElement("div");
-        container.appendChild(row).className = "gridRow";
-    };
+function makeRows(rows, cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    cell.innerText = (c + 1);
+    container.appendChild(cell).className = "grid-item";
+  };
 };
 
-// Creates columns
-function makeColumns(cellNum) {
-    console.log('bob2')
-    for (i = 0; i < rows.length; i++) {
-        for (j = 0; j < cellNum; j++) {
-            let newCell = document.createElement("div");
-            rows[j].appendChild(newCell).className = "cell";
-        };
-
-    };
-};
+makeRows(16, 16);
