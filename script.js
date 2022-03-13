0cc05d0150ce0148e4aed1236f1b3108220ce30c
Clement <perso@clementhurel.fr>
2022-03-13 19:17:41 +0900

    RPS

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ const container = document.getElementById("container");
 
 let gridSize = 16;
 
-function makeGrid(screen) {
-  for (let i = 0; i < screen ** 2; i++) {
-      const square = document.createElement("div");
+function makeGrid(screenSize) {
+  for (let i = 0; i < screenSize ** 2; i++) {
+      let square = document.createElement("div");
       square.classList.add('square');
       square.style.backgroundColor = 'blue';
       container.appendChild(square);
-      container.style.gridTemplateColumns = 'repeat($(screen), auto)';
-      container.style.gridTemplateRows = 'repeat($(screen), auto)';
   }
+  container.style.gridTemplateColumns = 'repeat($(screen), auto)';
+  container.style.gridTemplateRows = 'repeat($(screen), auto)';
 };
 
 makeGrid(gridSize);
