cea86d15cfb1335075a582670f3db33c1e45c721
Clement <perso@clementhurel.fr>
2022-03-13 18:22:56 +0900

    RPS

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const container = document.getElementById("container");
 
 function makeGrid(size) {
   for (let i = 0; i < size; i++) {
+    const square = container.createElement('square');
+    square.style.borderColor = 'black';
+    container.appendChild(square);
+
     for (let j=0;j < size; j++) {
       let square = document.createElement('div');
       square.className = 'square';
