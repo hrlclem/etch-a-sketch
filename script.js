4bd9c8f125965a9bbee5a8862dd6c30e1401a640
Clement <perso@clementhurel.fr>
2022-03-15 19:25:16 +0900

    RPS

--- script.js
+++ script.js
@@ -4,17 +4,19 @@ const clearBtn = document.querySelector(".clear");
 const eraseBtn = document.querySelector(".erase");
 const colorsBtn = document.querySelector(".colors");
 
-
-
-
-
+// Running it
 let gridSize = 16;
 makeGrid(gridSize); 
 blackColor();
 eraseAction();
+clearAction();
 const square = container.querySelectorAll('.square');
 
 
+
+
+
+// Create of X * X
 function makeGrid(gridSize) {
   for(let i = 0; i < gridSize; i++) {
     for(let j = 0; j < gridSize; j++) {
@@ -27,6 +29,7 @@ function makeGrid(gridSize) {
   }
 }
 
+// Change color to black
 function blackColor() {
   blackBtn.addEventListener('click', () => {
     square.forEach(square => square.addEventListener('mouseover', () => {
@@ -35,10 +38,16 @@ function blackColor() {
   })
 }
 
+// Change color to white
 function eraseAction() {
   eraseBtn.addEventListener('click', () => {
     square.forEach(square => square.addEventListener('mouseover', () => {
       square.style.background = 'white'; 
     }))
   })
+}
+
+// Clear whole grid and ask for size
+function clearAction() {
+
 }
