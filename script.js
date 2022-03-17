31b422d5315e8d6a14faa2dc7c3249f7d8ec5dc6
Clement <perso@clementhurel.fr>
2022-03-17 21:19:01 +0900

    RPS

--- script.js
+++ script.js
@@ -3,13 +3,17 @@ const blackBtn = document.querySelector(".black");
 const clearBtn = document.querySelector(".clear");
 const eraseBtn = document.querySelector(".erase");
 const colorsBtn = document.querySelector(".colors");
+const submitBtn = document.querySelector(".submit");
 const open = document.getElementById('open');
 const modal_container = document.getElementById('modal_container');
 const close = document.getElementById('close');
 
 
+
+
 // Running it
 let gridSize = 16;
+let slideAmount;
 container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
 container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
 
@@ -19,6 +23,21 @@ blackColor();
 eraseAction();
 rainbowColor();
 
+
+// Slider gridSize amount choice
+function updateSlider(slideAmount) {
+  var sliderDiv = document.getElementById("sliderAmount");
+  sliderDiv.innerHTML = slideAmount;
+  return slideAmount;
+}
+
+
+// Change gridSize amount
+function gridSizeChange() {
+  submitBtn.addEventListener('click', () => {
+    console.log(gridsize);
+  })
+}
 
 
 
